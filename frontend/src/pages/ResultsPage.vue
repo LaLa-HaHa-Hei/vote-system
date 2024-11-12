da8f0370a8b8f076003e9580c8611548277a86c4
LaLa-HaHa-Hei <mjcgc@qq.com>
2024-11-12 18:05:32 +0800

    数据可视化

--- frontend/src/pages/ResultsPage.vue
+++ frontend/src/pages/ResultsPage.vue
@@ -1,33 +1,48 @@
 <template>
     <header>投票结果</header>
-    <main>
-        <table border="1px solid #ccc" cellspacing="0" cellpadding="0">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>票数</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, index) in voteResults" :key="index">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.votes }}</td>
-                </tr>
-            </tbody>
-        </table>
+    <main style="width: 100%; height: 500px;">
     </main>
 </template>
 
 <script setup>
 import axios from 'axios'
-import { ref, onBeforeMount } from 'vue'
+import { ref, onMounted, nextTick } from 'vue'
+import * as echarts from 'echarts'
 
+// [{ id: '1', votes: 10 }, { id: '2', votes: 20 }, { id: '3', votes: 30 }]
 const voteResults = ref([])
 
-onBeforeMount(async () => {
+onMounted(async () => {
+    await nextTick()
     const { data } = await axios.get('/api/get-vote-results')
     voteResults.value = data.voteResults
+    var chartDom = document.querySelector('main');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    option = {
+        xAxis: {
+            type: 'category',
+            data: voteResults.value.map(item => item.id)
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: voteResults.value.map(item => item.votes),
+                type: 'bar'
+            }
+        ]
+    };
+    option && myChart.setOption(option);
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+header {
+    text-align: center;
+    font-size: 24px;
+    margin: 20px 0;
+}
+</style>
